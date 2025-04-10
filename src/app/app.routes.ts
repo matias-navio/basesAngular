70636111d763fc34326de28e938f1931eeed45fd
Matias Navio <naviomatias2@gmail.com>
2025-04-10 08:48:01 -0300

    Archivos que se quedaros sin pushear

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { DragonballComponent } from './pages/dragonball/dragonball.component';
 
 // rutas del sistema a las que podemos acceder y configurar
 export const routes: Routes = [
@@ -11,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroComponent
+  },
+  {
+    path: 'dragonball',
+    component: DragonballComponent
   }
 ];
